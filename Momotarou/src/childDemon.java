ed6412fadfa00371bcfa0091f785e1d25aeca0a1
harukasato0215 <popo02150125@gmail.com>
2024-02-05 18:09:06 +0900

    少し

--- Momotarou/src/childDemon.java
+++ Momotarou/src/childDemon.java
@@ -1,4 +1,14 @@
 
-public class childDemon {
+public class childDemon extends BattleCharacter{
+
+	String name = "小鬼";
+	int hp = 800;
+	
+	
+	@Override
+	public void attack() {
+	
+		
+	}
 
 }
---
+++ Momotarou/src/monkey.java
@@ -0,0 +1,10 @@
+
+public class monkey extends BattleCharacter{
+
+	@Override
+	public void attack() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
